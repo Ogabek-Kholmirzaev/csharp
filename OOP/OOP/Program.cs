83d7bfe64f9c73077c817af28db5c7174ab88e5e
Ogabek Kholmirzaev <105154244+Ogabek-Kholmirzaev@users.noreply.github.com>
2024-10-14 21:50:15 +0500

    Hierarchical Inheritance

--- OOP/OOP/Program.cs
+++ OOP/OOP/Program.cs
@@ -4,7 +4,12 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        var cola = new Soda("CocaCola", 0.9, "12/12/2019", 0.35, "Cola", false, true); //creation of Soda Object
-        cola.SodaDetails(); //calling method to print details
+        //creating and using the Beverage Object
+        var cola = new Beverage("RC Cola", 0.9, "12/12/2019", 350, "Cola"); 
+        cola.BeverageDetails();
+
+        //creating and using the Chocolate Object
+        var crunch = new Chocolate("Crunch", 2.3, "3/9/2019", 43, true);
+        crunch.ChocolateDetails();
     }
 }
