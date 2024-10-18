e051df148ccb56ebc6d919af839434dd59cf3ef8
Ogabek Kholmirzaev <105154244+Ogabek-Kholmirzaev@users.noreply.github.com>
2024-10-18 22:18:33 +0500

    SRP

--- SOLID/SOLID/Program.cs
+++ SOLID/SOLID/Program.cs
@@ -1,55 +1,19 @@
-﻿using System.Text;
-using System.Text.RegularExpressions;
-using System.Web;
+﻿using SOLID.SRP;
+
+#region SRP
 
 try
 {
-    Console.WriteLine("Please specify the full file path to convet to HTML.");
-
+    Console.WriteLine("Please specify the file to convert to HTML.");
     var filename = Console.ReadLine() ?? string.Empty;
-    var projectDirectory = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName;
+    var fileProcessor = new FileProcessor(filename);
+    var textProcessor = new TextProcessor(fileProcessor);
 
-    Console.WriteLine($"{projectDirectory}");
-
-    var fullFilePath = Path.Combine(projectDirectory, filename);
-
-    var inputText = ReadAllText(fullFilePath);
-
-    Console.WriteLine($"\n{inputText}\n");
-
-    var paragraphs = Regex.Split(inputText, @"(\r\n?|\n)")
-        .Where(x => x.Any(char.IsLetterOrDigit));
-
-    var stringBuilder = new StringBuilder();
-
-    foreach (var paragraph in paragraphs)
-    {
-        if (paragraph.Length == 0)
-        {
-            continue;
-        }
-
-        stringBuilder.AppendLine($"<p>{paragraph}</p>");
-    }
-    stringBuilder.AppendLine("<br/>");
-    WriteToFile(projectDirectory, filename, stringBuilder.ToString());
+    textProcessor.ConvertText();
 }
-catch(Exception exception)
+catch (Exception ex)
 {
-    Console.WriteLine(exception.Message);
+    Console.WriteLine(ex.Message);
 }
 
-string ReadAllText(string fullFilePath)
-{
-    return HttpUtility.HtmlEncode(File.ReadAllText(fullFilePath));
-}
-
-void WriteToFile(string projectDirectory, string fileName, string text)
-{
-    var outputFilePath = Path.Combine(projectDirectory, $"{Path.GetFileNameWithoutExtension(fileName)}.html");
-
-    Console.WriteLine(outputFilePath);
-
-    using StreamWriter file = new StreamWriter(outputFilePath);
-    file.Write(text);
-}
+#endregion
---
+++ SOLID/SOLID/SRP/TextProcessor.cs
@@ -0,0 +1,37 @@
+﻿using System.Text;
+using System.Text.RegularExpressions;
+
+namespace SOLID.SRP;
+
+public class TextProcessor
+{
+    private readonly FileProcessor fileProcessor;
+
+    public TextProcessor(FileProcessor fileProcessor)
+    {
+        this.fileProcessor = fileProcessor;
+    }
+
+    public void ConvertText()
+    {
+        var inputText = this.fileProcessor.ReadAllText();
+
+        Console.WriteLine(inputText);
+
+        var paragraphs = Regex.Split(inputText, @"(\r\n?|\n)").Where(x => x.Any(char.IsLetterOrDigit));
+        var stringBuilder = new StringBuilder();
+
+        foreach (var paragraph in paragraphs)
+        {
+            if (paragraph.Length == 0)
+            {
+                continue;
+            }
+
+            stringBuilder.AppendLine($"<p>{paragraph}</p>");
+        }
+
+        stringBuilder.AppendLine("<br/>");
+        this.fileProcessor.WriteToFile(stringBuilder.ToString());
+    }
+}
