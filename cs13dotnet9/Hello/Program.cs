357004627f007eb3430bdfa60043da66e4ea5cd2
Ogabek Kholmirzaev <105154244+Ogabek-Kholmirzaev@users.noreply.github.com>
2025-01-30 22:07:34 +0500

    Add Hello World project

---
+++ cs13dotnet9/Hello/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Namespace: {0}", typeof(Program).Namespace ?? "<null>");
+Console.WriteLine(Environment.CurrentDirectory);
+Console.WriteLine(Environment.OSVersion.VersionString);
