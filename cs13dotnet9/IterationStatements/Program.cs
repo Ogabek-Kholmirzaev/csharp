c45789244481431ee6c2a0d82528fdd6f9e08e97
Ogabek Kholmirzaev <105154244+Ogabek-Kholmirzaev@users.noreply.github.com>
2025-02-04 20:41:54 +0500

    Add IterationStatements project with initial implementation for demonstrating various iteration constructs in C#

---
+++ cs13dotnet9/IterationStatements/Program.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using static System.Console;
+
+#region while
+
+int x = 0;
+
+while (x < 10)
+{
+    WriteLine(x);
+    x++;
+}
+
+WriteLine();
+
+#endregion
+
+#region do while
+
+// string? actualPassword = "Pa$$w0rd";
+// string? password;
+// do
+// {
+//     Write("Enter your password: ");
+//     password = ReadLine();
+// } while (password != actualPassword);
+// WriteLine("Correct!\n");
+
+#endregion
+
+#region for
+
+for (int y = 1; y <= 10; y++)
+{
+    WriteLine(y);
+}
+
+for (int y = 0; y <= 10; y += 3)
+{
+    WriteLine(y);
+}
+
+WriteLine();
+
+#endregion
+
+#region foreach
+
+List<string> names = [ "Adam", "Barry", "Charlie" ];
+foreach (string name in names)
+{
+    WriteLine($"{name} has {name.Length} characters.");
+}
+
+IEnumerator e = names.GetEnumerator();
+while (e.MoveNext())
+{
+  string name = (string)e.Current; // Current is read-only!
+  WriteLine($"{name} has {name.Length} characters.");
+}
+
+WriteLine();
+
+#endregion
