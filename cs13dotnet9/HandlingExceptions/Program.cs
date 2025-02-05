8956a1d6d2baad945b514f2f5700f54eb3f47b65
Ogabek Kholmirzaev <105154244+Ogabek-Kholmirzaev@users.noreply.github.com>
2025-02-06 04:07:36 +0500

    Add HandlingExceptions project with initial implementation for demonstrating exception handling in C#

---
+++ cs13dotnet9/HandlingExceptions/Program.cs
@@ -0,0 +1,49 @@
+﻿using static System.Console;
+
+WriteLine("Before parsing");
+Write("What is your age? ");
+
+string? input = ReadLine();
+try
+{
+    int age = int.Parse(input!);
+    WriteLine($"You are {age} years old.");
+}
+catch (OverflowException)
+{
+    WriteLine("Your age is a valid number format but it is either too big or small.");
+}
+catch (FormatException)
+{
+    WriteLine("The age you entered is not a valid number format.");
+}
+catch (Exception ex)
+{
+    WriteLine($"{ex.GetType()} says {ex.Message}");
+}
+WriteLine("After parsing\n");
+
+#region catching with filters
+
+Write("Enter an amount: ");
+string amount = ReadLine()!;
+if (string.IsNullOrEmpty(amount))
+{
+    return;
+}
+
+try
+{
+    decimal amountValue= decimal.Parse(amount);
+    WriteLine($"Amount formatted as currency: {amount:C}");
+}
+catch (FormatException) when (amount.Contains('$'))
+{
+    WriteLine("Amounts cannot use the dollar sign!");
+}
+catch (FormatException)
+{
+    WriteLine("Amountsmust only contain digits!");
+}
+
+#endregion
