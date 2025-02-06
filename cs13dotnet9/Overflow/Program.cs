a167a396063b661ebf3df97bf414a93fb8a4b6c4
Ogabek Kholmirzaev <105154244+Ogabek-Kholmirzaev@users.noreply.github.com>
2025-02-06 14:41:15 +0500

    Add Overflow project with initial implementation demonstrating integer overflow behavior in C#

---
+++ cs13dotnet9/Overflow/Program.cs
@@ -0,0 +1,87 @@
+﻿using static System.Console;
+
+#region overflow
+
+/*
+Once it gets above its maximum value,
+it overflows to its minimum value and continues incrementing from there.
+*/
+
+int x = int.MaxValue - 1;
+WriteLine($"Initial value: {x}");
+
+x++;
+WriteLine($"After incrementing: {x}");
+
+x++;
+WriteLine($"After incrementing: {x}");
+
+x++;
+WriteLine($"After incrementing: {x}");
+
+WriteLine();
+
+#endregion
+
+#region checked
+
+/*
+If overflow happens, the exception is thrown (OverflowException)
+*/
+
+checked
+{
+    x = int.MaxValue - 1;
+    WriteLine($"Initial value: {x}");
+
+    x++;
+    WriteLine($"After incrementing: {x}");
+
+    x++;
+    WriteLine($"After incrementing: {x}");
+
+    x++;
+    WriteLine($"After incrementing: {x}");
+}
+
+try
+{
+  // previous code goes here
+}
+catch (OverflowException)
+{
+    WriteLine("The code overflowed but I caught the exception.");
+}
+
+WriteLine();
+
+#endregion
+
+#region unchecked
+
+// used to disable compile-time overflow check
+unchecked
+{
+    int y = int.MaxValue + 1;
+    WriteLine($"Initial value: {y}");
+    
+    y--;
+    WriteLine($"After decrementing: {y}");
+    
+    y--;
+    WriteLine($"After decrementing: {y}");
+
+    WriteLine();
+}
+
+#endregion
+
+#region loops and overflow
+
+int max = 500;
+for (byte i = 0; i < max; i++)
+{
+    WriteLine(i);
+}
+
+#endregion
