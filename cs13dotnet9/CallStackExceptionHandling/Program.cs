c0f77fe6e65133aac9374759b47396c86ba430d8
Ogabek Kholmirzaev <105154244+Ogabek-Kholmirzaev@users.noreply.github.com>
2025-02-08 02:01:49 +0500

    Add CallStackExceptionHandling project with initial implementation for exception handling

---
+++ cs13dotnet9/CallStackExceptionHandling/Program.cs
@@ -0,0 +1,42 @@
+﻿using CallStackExceptionHandlingLib;
+using static System.Console;
+
+WriteLine("In Main");
+Alpha();
+
+void Alpha()
+{
+    WriteLine("In Alpha");
+    Beta();
+}
+
+void Beta()
+{
+    WriteLine("In Beta");
+
+    try
+    {
+        Processor.Gamma();
+    }
+    catch (IOException ex)
+    {
+        LogException(ex);
+
+        // Throw the caught exception as if it happened here
+        // this will lose the original call stack.
+        //throw ex;
+
+        // Rethrow the caught exception and retain its original call stack.
+        //throw;
+
+        // Throw a new exception with the caught exception nested within it.
+        throw new InvalidOperationException(
+            message: "Calculation had invalid values. See inner exception for why.",
+            innerException: ex);
+    }
+}
+
+void LogException(Exception ex)
+{
+    WriteLine($"{DateTime.Now}\tMessage: {ex.Message}");
+}
